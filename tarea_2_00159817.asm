08e6c787a36d6f6bd0cebfd21c4bfa320f36334e
NitroSkate <00159817@uca.edu.sv>
2020-04-25 01:07:51 -0600

    Terminado, ejercicios comentados

--- tarea_2_00159817.asm
+++ tarea_2_00159817.asm
@@ -1,4 +1,60 @@
-org	100h	
+;Ejercicio 1
+/*org	100h
+mov	ax, 5d
+add	ax, 9d
+add	ax, 8d
+add     ax, 1d
+add	ax, 7d
+mov 	bx, 5d
+div	bx
+
+mov 	cl, "P"
+mov	[200h], cl
+mov 	cl, "e"
+mov	[201h], cl
+mov 	cl, "o"
+mov	[202h], cl
+mov 	cl, "r"
+mov	[203h], cl
+mov 	cl, "e"
+mov	[204h], cl
+mov 	cl, "s"
+mov	[205h], cl
+mov 	cl, "n"
+mov	[206h], cl
+mov 	cl, "a"
+mov	[207h], cl
+mov 	cl, "d"
+mov	[208h], cl
+mov 	cl, "a"
+mov	[209h], cl
+
+int	20h*/
+
+;Ejercicio 2
+/*org	100h
+mov	ax, 4d
+mov	si, 0d
+mov 	bx, 2d
+
+lp1: mov	[si + 210h], ax
+mul	bx
+inc	si
+cmp	AX, 100h
+jb	lp1
+
+lp2: mov	[si + 210h], ax
+mul	bx
+inc	si
+inc	si
+cmp	AX, 2000h
+jb	lp2
+
+
+int	20h*/
+
+;Ejercicio 3
+/*org	100h
 mov	ax, 0d
 mov	bx, 1d
 mov	cx, 0d
@@ -6,19 +62,20 @@ mov	si, 0h
 mov	[si + 220h], ax
 loop1:inc	si
 mov	cx, ax
-add	ax, bx
+add	ax,bx
 mov	[si + 220h], ax
-mov	bx, cx
+mov 	bx,cx
 cmp	ax, 233d
 jb	loop1
 loop2:inc	si
 mov	cx, ax
-add	ax, bx
+add	ax,bx
 mov	[si + 220h], al
-inc	si
+inc 	si
 mov	[si + 220h], ah
-mov	bx, cx
+mov 	bx,cx
 cmp	si, 10h
 jb	loop2
 
-int 	20h
+
+int	20h*/
