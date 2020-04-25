963d8e221b2cc686c95a5dbb7f87edf171742989
NitroSkate <00159817@uca.edu.sv>
2020-04-24 23:30:19 -0600

    Ejercicio 3 terminado

--- tarea_2_00159817.asm
+++ tarea_2_00159817.asm
@@ -1,20 +1,24 @@
-org	100h
-mov	ax, 4d
-mov	si, 0d
-mov	bx, 2d
-
-lp1: mov	[si + 210h], ax
-mul	bx
+org	100h	
+mov	ax, 0d
+mov	bx, 1d
+mov	cx, 0d
+mov	si, 0h
+mov	[si + 220h], ax
+loop1:inc	si
+mov	cx, ax
+add	ax, bx
+mov	[si + 220h], ax
+mov	bx, cx
+cmp	ax, 233d
+jb	loop1
+loop2:inc	si
+mov	cx, ax
+add	ax, bx
+mov	[si + 220h], al
 inc	si
-cmp	ax, 100h
-jb	lp1
+mov	[si + 220h], ah
+mov	bx, cx
+cmp	si, 10h
+jb	loop2
 
-lp2: mov	[si + 210h], ax
-mul	bx	
-inc 	si
-inc	si
-cmp	ax, 2000h
-jb	lp2
-
-int	20h
-
+int 	20h
