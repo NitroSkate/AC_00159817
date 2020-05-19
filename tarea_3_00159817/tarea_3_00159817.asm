83b5d8d1680d46c25217132b6bab411b7c95ee90
NitroSkate <00159817@uca.edu.sv>
2020-05-18 20:26:30 -0600

    Terminado

--- tarea_3_00159817/tarea_3_00159817.asm
+++ tarea_3_00159817/tarea_3_00159817.asm
@@ -5,6 +5,8 @@ section .text
 	call	texto
 	call	cursor
 	call	phrase
+	call	phrase2
+	call	phrase3
 	call	kbwait
 
 	int	20h
@@ -40,6 +42,20 @@ m_cursr: mov	ah, 02h
 	int	10h
 	ret
 
+m_cursr2: mov	ah, 02h
+	mov	dx, di
+	mov	dh, 12d
+	mov	bh, 0h
+	int	10h
+	ret
+
+m_cursr3: mov	ah, 02h
+	mov	dx, di
+	mov	dh, 18d
+	mov	bh, 0h
+	int	10h
+	ret
+
 phrase: mov	di, 17d
 lupi:  	mov	cl, [msg+di-17d]
 	call	m_cursr
@@ -49,6 +65,31 @@ lupi:  	mov	cl, [msg+di-17d]
 	jb	lupi
 	ret
 
+phrase2: mov	di, 07d
+lupi2:  	mov	cl, [msg2+di-07d]
+	call	m_cursr2
+	call	w_char
+	inc	di
+	cmp	di, len2
+	jb	lupi2
+	ret
+
+phrase3: mov	di, 27d
+lupi3:  	mov	cl, [msg3+di-27d]
+	call	m_cursr3
+	call	w_char
+	inc	di
+	cmp	di, len3
+	jb	lupi3
+	ret
+
 section .data
 msg	db	"This right here is astronomical"
 len	equ	$-msg+17d
+
+msg2	db	"I see you picked up all my ways, I feel responsible"
+len2	equ	$-msg2+07d
+
+msg3	db	"They tryna say that all my problems is improbable"
+len3	equ	$-msg3+27d
+
