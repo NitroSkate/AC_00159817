dd931903ff0971d0d2d9d37fa7c7ef4d2aa22018
NitroSkate <00159817@uca.edu.sv>
2020-05-18 20:11:55 -0600

    Preparando la base para el ejercicio

---
+++ tarea_3_00159817/tarea_3_00159817.asm
@@ -0,0 +1,54 @@
+	org	100h	
+
+section .text
+
+	call	texto
+	call	cursor
+	call	phrase
+	call	kbwait
+
+	int	20h
+
+texto:	mov	ah, 00h
+	mov	al, 03h
+	int	10h
+	ret
+
+cursor:	mov	ah, 01h
+	mov	ch, 00000000b
+	mov	cl, 00001110b
+		;  IRGB
+	int	10h
+	ret
+
+w_char: mov	ah, 09h
+	mov	al, cl
+	mov	bh, 0h
+	mov	bl, 00001111b
+	mov	cx, 1h
+	int	10h
+	ret
+
+kbwait: mov	ax, 0000h
+	int	16h
+	ret
+
+m_cursr: mov	ah, 02h
+	mov	dx, di
+	mov	dh, 6d
+	mov	bh, 0h
+	int	10h
+	ret
+
+phrase: mov	di, 17d
+lupi:  	mov	cl, [msg+di-17d]
+	call	m_cursr
+	call	w_char
+	inc	di
+	cmp	di, len
+	jb	lupi
+	ret
+
+section .data
+msg	db	"This right here is astronomical"
+len	equ	$-msg+17d
